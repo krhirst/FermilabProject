94249753e2dfda77cd607ebedcbf6bc13880be9e
Jacket23 <thezan234@yahoo.com>
2019-12-01 21:33:47 -0600

    Merge branch 'new-gui' into reports

--- src/reportPage/ReportsController.java
+++ src/reportPage/ReportsController.java
@@ -5,6 +5,7 @@ import application.FermiEntry;
 import editUsersPage.EditUsersView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.print.*;
 import javafx.scene.Node;
@@ -13,14 +14,17 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 import login.LoginController;
 
-import java.awt.*;
+import javafx.scene.control.TextField;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ReportsController {
     private FermiConnector base = new FermiConnector();
@@ -51,6 +55,18 @@ public class ReportsController {
     
     @FXML
     private Button backButton;
+    
+    @FXML
+    private Button bisonButton;
+    
+    @FXML
+    private Button seniorityButton;
+    
+    @FXML
+    private TextField searchNumber;
+    
+    @FXML
+    private Text errorText;
 
     public ReportsController() {
 
@@ -64,25 +80,6 @@ public class ReportsController {
         overCol.setCellValueFactory(new PropertyValueFactory("overtime"));
         senCol.setCellValueFactory(new PropertyValueFactory("seniority"));
         bisonCol.setCellValueFactory(new PropertyValueFactory("inBison"));
-
-        dataTable.setItems(getData());
-    }
-
-    private ObservableList<FermiEntry> getData() {
-        ObservableList<FermiEntry> data = FXCollections.observableArrayList();
-
-        try {
-            Statement stmt = base.getConn().createStatement();
-            ResultSet result = stmt.executeQuery("SELECT * FROM hours_offered");
-            while (result.next()) {
-                data.add(new FermiEntry(result.getString(1), result.getString(2), result.getString(3), result.getDouble(4),
-                        result.getInt(5), result.getBoolean(6)));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return data;
     }
 
     @FXML
@@ -118,20 +115,105 @@ public class ReportsController {
             root.getTransforms().remove(scale);
         }
     }
-
-    @FXML
-    private void goBack() throws Exception {
-        Stage stage = (Stage) backButton.getScene().getWindow();
-        
-        EditUsersView view = new EditUsersView();
-        view.showView(stage);
-    }
     
     @FXML
-    private void editUsers() throws Exception{
+    private void goBack() throws Exception{
     	Stage stage = (Stage) backButton.getScene().getWindow();
 
         EditUsersView view = new EditUsersView();
         view.showView(stage);
     }
+    
+    @FXML
+    private void BisonReport() throws Exception{
+    	
+    	dataTable.getItems().clear();
+    	
+    	//grabbing list of employees from database
+    	ObservableList<FermiEntry> employees = getEmployees();
+    	ObservableList<FermiEntry> bisonEmployees = FXCollections.observableArrayList();
+    	
+    	//counter for the current index in employees
+    	int emp = 0;
+    	
+    	for(FermiEntry e: employees) {
+    		if(e.isInBison()) {
+    			bisonEmployees.add(e);
+    		}
+    	}
+    	
+    	//bubble sort
+    	for (int i = 0; i < bisonEmployees.size()-1; i++)   {   
+    		emp = 0;
+    	    for (int j = 0; j < bisonEmployees.size()-i-1; j++){
+    	    	//compare the last names of the current employee and the next one in the list
+    	        if (compare(bisonEmployees.get(j).getLastName(), bisonEmployees.get(j+1).getLastName()) >= 1) { 
+    	            swap(bisonEmployees.get(j), bisonEmployees.get(j+1), emp, bisonEmployees);
+    	        }
+    	        emp++;
+    		}
+    	}
+    	
+    	dataTable.setItems(bisonEmployees);
+    }
+    
+    private void swap(FermiEntry fermiEntry, FermiEntry fermiEntry2, int emp, ObservableList<FermiEntry> employees) {
+		
+    	FermiEntry temp = new FermiEntry(fermiEntry.getFirstName(), fermiEntry.getLastName(), fermiEntry.getPhone(), fermiEntry.getOvertime(),
+                fermiEntry.getSeniority(), fermiEntry.isInBison());
+		
+		employees.set(emp, fermiEntry2);
+		
+		employees.set(emp+1, temp);
+		
+	}
+	
+    
+    private int compare(String aLastName, String bLastName) {
+		return aLastName.compareTo(bLastName);
+	}
+
+	@FXML
+    private void SeniorReport() throws Exception{
+		
+		int searchNum;
+		errorText.setVisible(false);
+		
+    	try {
+    		searchNum = Integer.parseInt(searchNumber.getText());
+    	}
+    	catch (NumberFormatException e) {
+    		errorText.setVisible(true);
+    		return;
+    	}
+    	
+    	ObservableList<FermiEntry> employees = getEmployees();
+    	ObservableList<FermiEntry> SeniorityEmployees = FXCollections.observableArrayList();
+    	
+    	for(FermiEntry e: employees) {
+    		if(e.getSeniority() == searchNum) {
+    			SeniorityEmployees.add(e);
+    		}
+    	}
+    	
+    	dataTable.setItems(SeniorityEmployees);
+    }
+
+	private ObservableList<FermiEntry> getEmployees() {
+		
+		ObservableList<FermiEntry> employees = FXCollections.observableArrayList();
+		
+		try {
+            Statement stmt = base.getConn().createStatement();
+            ResultSet result = stmt.executeQuery("SELECT * FROM hours_offered");
+            while (result.next()) {
+                employees.add(new FermiEntry(result.getString(1), result.getString(2), result.getString(3), result.getDouble(4),
+                        result.getInt(5), result.getBoolean(6)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+		
+		return employees;
+	}
 }
